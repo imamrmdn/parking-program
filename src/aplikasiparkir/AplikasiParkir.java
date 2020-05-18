5e9aa0a5b6e06f64b8ed58078ad241fd7248ae4f
imamrmdn <imamramadhan420@gmail.com>
2020-05-19 03:51:14 +0700

    initial commit

---
+++ src/aplikasiparkir/AplikasiParkir.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasiparkir;
+
+/**
+ *
+ * @author X550ZE
+ */
+public class AplikasiParkir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_AplikasiParkir Laund = new Frame_AplikasiParkir();
+        Laund.setVisible(true);
+        
+    }
+    
+}
